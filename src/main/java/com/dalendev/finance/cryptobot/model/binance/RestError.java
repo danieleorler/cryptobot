cb2d5458d690ddf87ba978d5ab2430473a719dab
Daniele Orler <daniele.orler@gmail.com>
2018-01-10 20:40:22 +0100

    binance error entity

---
+++ src/main/java/com/dalendev/finance/cryptobot/model/binance/RestError.java
@@ -0,0 +1,26 @@
+package com.dalendev.finance.cryptobot.model.binance;
+
+/**
+ * @author daniele.orler
+ */
+public class RestError {
+
+    private int code;
+    private String msg;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
