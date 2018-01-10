ce472f114ad165c055c06bb48f11a5b8274f59d1
Daniele Orler <daniele.orler@gmail.com>
2018-01-10 20:41:25 +0100

    binance signature

--- src/main/java/com/dalendev/finance/cryptobot/adapters/rest/binance/BaseAdapter.java
+++ src/main/java/com/dalendev/finance/cryptobot/adapters/rest/binance/BaseAdapter.java
@@ -1,10 +1,54 @@
 package com.dalendev.finance.cryptobot.adapters.rest.binance;
 
+import com.dalendev.finance.cryptobot.model.exceptions.EncodingException;
+import org.apache.commons.codec.binary.Hex;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.stream.Collectors;
+
 /**
  * @author daniele.orler
  */
 public class BaseAdapter {
 
     protected final String baseUrl = "https://api.binance.com";
+    protected final RestTemplate restTemplate;
 
+    @Value("${binance.api_key}")
+    protected String apiKey;
+
+    @Value("${binance.api_secret}")
+    private String apiSecret;
+
+    @Autowired
+    public BaseAdapter(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    protected MultiValueMap<String, String> sign(MultiValueMap<String, String> params) {
+
+        String mergedParams = params.toSingleValueMap().entrySet().stream()
+            .map(entry -> entry.getKey() + "=" + entry.getValue())
+            .collect(Collectors.joining("&"));
+
+        params.add("signature", encode(apiSecret, mergedParams));
+
+        return params;
+    }
+
+    private String encode(String key, String data) {
+        try {
+            Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+            SecretKeySpec secret_key = new SecretKeySpec(key.getBytes("UTF-8"), "HmacSHA256");
+            sha256_HMAC.init(secret_key);
+            return Hex.encodeHexString(sha256_HMAC.doFinal(data.getBytes("UTF-8")));
+        } catch(Exception e) {
+            throw new EncodingException(e);
+        }
+    }
 }
