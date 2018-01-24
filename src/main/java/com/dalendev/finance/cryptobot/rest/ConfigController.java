42d67ef7f9003cf9d1b6011b9d4a93ddcd15e9ad
Rein Toompuu <rein@toompuu.se>
2018-01-24 22:40:19 +0100

    added runtime updatable config

---
+++ src/main/java/com/dalendev/finance/cryptobot/rest/ConfigController.java
@@ -0,0 +1,36 @@
+package com.dalendev.finance.cryptobot.rest;
+
+import com.dalendev.finance.cryptobot.services.ConfigService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/v1/api/config")
+@CrossOrigin(origins = "*")
+public class ConfigController {
+
+    @Autowired
+    private final ConfigService configService;
+
+
+    public ConfigController(ConfigService configService) {
+        this.configService = configService;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public Map<String, String> getAllProperties() {
+        return configService.getUpdatableConfig();
+    }
+
+    @RequestMapping(value = "/{key}", method = RequestMethod.GET)
+    public String getProperty(@PathVariable String key) {
+        return configService.getProperty(key);
+    }
+
+    @RequestMapping(value = "/{key}", method = RequestMethod.POST)
+    public void updateProperty(@PathVariable String key, @RequestBody String value) {
+        configService.updateProperty(key, value);
+    }
+}
